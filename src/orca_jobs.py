8d7787767781ec9836c5efaeeb11d94b92dcef37
pmwaddell <pmwaddell9@gmail.com>
2024-11-19 14:51:16 -0500

    Add some logs, update orca_jobs to skip trajectory xyz files when looking for inputs

--- src/orca_jobs.py
+++ src/orca_jobs.py
@@ -63,6 +63,10 @@ def orca_batch_job(path_to_xyz_files: str, destination_path: str, job_type: str,
         # Basically, for all intents and purposes, CID herein just means the unique label for this molecule.
         # It wouldn't have to be an actual CID.
         cid = xyz_file_path.split("/")[-1][:-4]
+
+        # Skip trajectory .xyz files, which are produced by geometry opts. They end in _trj.xyz.
+        if cid[-4:] == "_trj":
+            continue
         cids.append(cid)
 
         try:
@@ -117,30 +121,35 @@ def orca_batch_job(path_to_xyz_files: str, destination_path: str, job_type: str,
         subprocess.run(orca_command, shell=True)
         print(f"complete. Total time: {datetime.timedelta(seconds=time.time() - start)}")
         log += f"{job_type} for {cid} completed at {datetime.datetime.now()}\n"
-        with open("logs/log.txt", "w") as log_file:
+        with open("logs/NiCO2_geom_opt_log.txt", "w") as log_file:
             log_file.write(log)
 
     print(f"\n{job_type}s complete.")
     log += f"\n{job_type} ended {datetime.datetime.now()}\n"
-    with open("logs/log.txt", "w") as log_file:
+    with open("logs/NiCO2_geom_opt_log.txt", "w") as log_file:
         log_file.write(log)
 
 
 if __name__ == "__main__":
-    print("Ligand geometry optimizations: ")
-    orca_batch_job(path_to_xyz_files="data/diimine_data/conf_search_ligand",
-                   destination_path="data/diimine_data/geom_opt_ligand_BP86",
-                   functional="BP86",
-                   basis_set="def2-SVP",
-                   RI='RI',
-                   dispersion_correction="D3BJ",
-                   job_type="Geometry Optimization")
-
-    print("\n\nNi carbonyl complex geometry optimizations: ")
-    orca_batch_job(path_to_xyz_files="data/diimine_data/conf_search_NiCO2",
-                   destination_path="data/diimine_data/geom_opt_NiCO2",
+    print("\n\nNi ethylene complex single point calcs: ")
+    orca_batch_job(path_to_xyz_files="data/diimine_data/geom_opt_NiE",
+                   destination_path="data/diimine_data/single_pt_NiE",
                    functional="B3LYP",
                    basis_set="def2-TZVP",
                    RI='RIJCOSX',
                    dispersion_correction="D3BJ",
-                   job_type="Geometry Optimization")
+                   charge=1,
+                   NMR=True,
+                   freq=True,
+                   job_type="Single Point Calculation")
+
+    print("\n\nNiCO2 complex single point calcs: ")
+    orca_batch_job(path_to_xyz_files="data/diimine_data/geom_opt_NiCO2",
+                   destination_path="data/diimine_data/single_pt_NiCO2",
+                   functional="B3LYP",
+                   basis_set="def2-TZVP",
+                   RI='RIJCOSX',
+                   dispersion_correction="D3BJ",
+                   NMR=True,
+                   freq=True,
+                   job_type="Single Point Calculation")
